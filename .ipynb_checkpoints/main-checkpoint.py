734927c6305e3986e810571190ee4c25282e5c25
Tangent <nt108021223hu@gapp.nthu.edu.tw>
2024-06-26 12:10:17 +0800

    add files

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,9 @@
+import time
+import joblib
+
+from src.function import function
+
+if __name__ == "__main__":
+    print(' ⛾ Welcome to GCBICT! ⛾ ')
+    print('⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺')
+    function()
